d069a49c124fbed46914d882c50b865cc5107fbc
kari0003 <kari1038@gmail.com>
2016-06-16 23:27:53 +0200

    mockup for frontend
    mockup for alpha version frontend

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
-import { makeRequest } from './config/requestUtil.js';
+import { makeRequest } from './utils.js';
 import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+
+  app.use(express.static(__dirname + '/public'));
+  app.use(morgan('dev'));
+  app.use(bodyParser.urlencoded({'extended':'true'}));
+  app.use(bodyParser.json());
+
+
 app.get('/', function(req, res) {
-  makeRequest('', 0, get, {})
+  res.sendfile('./src/public/index.html');
+});
+
+app.post('/api', function(req, res) {
+  console.log(req.body);
+  makeRequest('', 0, 'get', {})
   .then((data) => {
-    res.json(data);
+    res.json({
+      useful: 'this is a legit message',
+      data: data,
+    });
   })
   .catch((err) => {
     console.log(err);
     throw new Error();
   });
 });
-
 app.listen(3000, () => {
   console.log('Started up!');
 });
-
-/*
-req('http://www.google.com', function (err, res, body) {
-  if (!err && res.statusCode == 200) {
-    console.log(body) // Print the google web page.
-  }
-})
-*/
